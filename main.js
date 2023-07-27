d7bac7f119c387f74cf1114da1d1a2c90c31efb8
AmruteAarya <87522584+AmruteAarya@users.noreply.github.com>
2023-07-27 14:25:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+pop_music="";
+happy_tunes_music="";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+function preload(){
+    pop_music = loadSound("pop_music.mp3");
+    Harry_potter_theme_song = loadSound("happy_tunes.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
